066a6102d8bacae10dfba8ee3209e93e860cb7e3
Vladimir Morozov <vmorozov@microsoft.com>
2022-03-08 14:49:34 -0800

    Change comment

--- test/js-native-api/test_finalizing_queue/test_sync_throw.js
+++ test/js-native-api/test_finalizing_queue/test_sync_throw.js
@@ -3,6 +3,8 @@
 
 // The test verifies that the finalizing queue is drained synchronously
 // when we call a method that may cause a GC call.
+// This method throws a JS exception when a finalizer from the queue
+// throws a JS exception.
 
 const common = require('../../common');
 const assert = require('assert');
@@ -25,11 +27,14 @@ async function runGCTests() {
     test.createObject(/*throw on destruct*/true);
   })();
   global.gc();
+  // The finalizing queue processing is stopped on the first JS exception.
   assert.throws(() => test.drainFinalizingQueue(), {
     name: 'Error',
     message: 'Error during Finalize'
   });
   assert.strictEqual(test.finalizeCount, 2);
+  // To handle the next items in the the finalizing queue we
+  // must call a GC-touching function again.
   assert.throws(() => test.drainFinalizingQueue(), {
     name: 'Error',
     message: 'Error during Finalize'
